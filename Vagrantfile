bfd256af4bdb8283bcc0229db9e5ac5078d84007
Vladislav <farmatholin@gmail.com>
2015-11-14 03:32:48 +0300

    Reformat container.yml Update Vagrantfile Update pgluster "Upload big file bug" - fixed

--- Vagrantfile
+++ Vagrantfile
@@ -12,50 +12,44 @@ require 'yaml'
 # Be sure to edit 'containers.yml' to provide container details
 containers = YAML.load_file('containers.yml')
 
+container_data = containers["data"]
+server = container_data ["server"]
+client = container_data["client"]
+web_proxy = container_data["web"]
+
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-    containers["data"].each do |container|
-        (1..container["count"]).each do |i|
-            config.vm.define vm_name = "#{container["name"]}-#{i}" do |config|
-                config.vm.synced_folder ".", "/vagrant", disabled: true
-                config.vm.provider "docker" do |d|
-                    config.vm.hostname = vm_name
-                    d.build_dir = container["dir"]
-                    d.privileged = true
-                    if container["type"] != 'server'
-                        d.name = container["name"]
-                        d.expose = [8000]
-                        d.cmd = ["/usr/local/bin/gunicorn", "-w 2", "-b :8000", "app:app"]
-                    else
-                        d.name = vm_name
-                        d.volumes = ["/#{containers["gluster"]["gluster_brick"]}_#{i}:/#{containers["gluster"]["gluster_brick"]}"]
-                    end
-                end
+    (1..server["count"]).each do |i|
+        config.vm.define vm_name = "#{server["name"]}-#{i}" do |config|
+            config.vm.synced_folder ".", "/vagrant", disabled: true
+            config.vm.provider "docker" do |d|
+                config.vm.hostname = vm_name
+                d.remains_running = true
+                d.build_dir = server["dir"]
+                d.privileged = true
+                d.name = vm_name
+                d.volumes = ["/#{containers["gluster"]["gluster_brick"]}_#{i}:/#{containers["gluster"]["gluster_brick"]}"]
             end
         end
     end
-    #config.vm.define "web" do |web|
-        #web.vm.synced_folder ".", "/vagrant", disabled: true
-        #web.vm.provider "docker" do |d|
-            #d.remains_running = true
-            #d.build_dir = "client/app"
-            #d.name = "web-gluster-client"
-            #d.expose = [8000]
-            #(1..clients).each do |i|
-                #d.link("#{client_name}-#{i}:#{client_name}-#{i}")
-            #end
-            #d.link("glusterfs-client:glusterfs-client")
-            #d.has_ssh = false
-            #d.create_args = ["--volumes-from=glusterfs-client"]
-            #d.cmd = ["/usr/local/bin/gunicorn", "-w 2", "-b :8000", "app:app"]
-       # end
-     #end
+    config.vm.define "web" do |web|
+        web.vm.synced_folder ".", "/vagrant", disabled: true
+        web.vm.provider "docker" do |d|
+            d.privileged = true
+            d.remains_running = true
+            d.build_dir = client["dir"]
+            d.name = client["name"]
+            d.expose = [8000]
+            d.has_ssh = false
+            d.cmd = ["/usr/local/bin/gunicorn", "-w #{client["workers"]}", "-b :8000", "app:app"]
+        end
+    end
     config.vm.define "nginx" do |nginx|
         nginx.vm.synced_folder ".", "/vagrant", disabled: true
         nginx.vm.provider "docker" do |d|
-            d.build_dir = "client/nginx"
-            d.name = "nginx-gluster"
-            d.ports = ["9092:80"]
-            d.link("glusterfs-client:glusterfs-client")
+            d.build_dir = web_proxy["dir"]
+            d.name = web_proxy['name']
+            d.ports = ["#{web_proxy["port"]}:80"]
+            d.link("#{client["name"]}:#{client["name"]}")
             d.remains_running = true
             d.has_ssh = false
         end
--- pgluster.py
+++ pgluster.py
@@ -22,12 +22,16 @@ VAGRANT = 'vagrant'
 DOCKER = 'docker'
 
 
-def manage_script(args):
+def manage_script(args, print_output=False, print_errors=False):
     sleep(2)
     child = Popen(args, stdout=PIPE, stderr=PIPE)
+
     output = child.stdout.read()
     err = child.stderr.read()
-    print output
+    if print_output:
+        print output
+    if print_errors:
+        print err
 
     child.communicate()
     rc = child.returncode
@@ -55,7 +59,6 @@ def link_servers(containers, gluster_data):
                    .replace('\'', '')
                    )
 
-    print ips
     peer_command = ''
     start_command = 'gluster volume start ' + gluster_data['gluster_vol']
     connect_command = 'gluster volume create ' \
@@ -71,13 +74,13 @@ def link_servers(containers, gluster_data):
 
     first_name = containers['name'] + '-' + '1'
 
-    rc = manage_script([DOCKER, 'exec', first_name, '/bin/sh', '-c', peer_command])
+    rc = manage_script([DOCKER, 'exec', first_name, '/bin/sh', '-c', peer_command],True)
     print "connecting peers return code = %d" % rc
 
-    rc = manage_script([DOCKER, 'exec', first_name, '/bin/sh', '-c', connect_command])
+    rc = manage_script([DOCKER, 'exec', first_name, '/bin/sh', '-c', connect_command],True)
     print "connecting volumes return code = %d" % rc
 
-    rc = manage_script([DOCKER, 'exec', first_name, '/bin/sh', '-c', start_command])
+    rc = manage_script([DOCKER, 'exec', first_name, '/bin/sh', '-c', start_command],True)
     print "start command return code = %d" % rc
 
     return ips[0]
@@ -86,22 +89,22 @@ def link_servers(containers, gluster_data):
 def connect_client(server, client, gluster_data):
     run_command = "mount -t glusterfs " + server + ":/" + gluster_data['gluster_vol'] + " /mnt/glusterfs"
 
-
-    rc = manage_script([DOCKER, 'exec', "glusterfs-client", '/bin/sh', '-c', run_command])
+    rc = manage_script([DOCKER, 'exec', client['name'], '/bin/sh', '-c', run_command],True)
     print "connect return code = %d" % rc
+    return rc
 
 
 def stop_all(containers):
-    for item in containers['data']:
-        i = 0
-        while i < item['count']:
-            i += 1
-            container_name = item['name'] + "-" + str(i)
-            manage_script([DOCKER, 'rm', '-f', container_name])
-        #manage_script([DOCKER, 'rm', '-f', "web-gluster-client"])
-        manage_script([DOCKER, 'rm', '-f', "glusterfs-client"])
-        manage_script([DOCKER, 'rm', '-f', "nginx-gluster"])
-    return 1
+    manage_script([VAGRANT, 'halt', '-f'],True)
+
+    servers = containers['data']['server']
+    i = 0
+    while i < servers['count']:
+        i += 1
+        container_name = servers['name'] + "-" + str(i)
+        manage_script([DOCKER, 'rm', '-f', container_name])
+    manage_script([DOCKER, 'rm', '-f', containers['data']['client']['name']])
+    manage_script([DOCKER, 'rm', '-f', containers['data']['web']['name']])
 
 
 def get_containers_data():
@@ -110,31 +113,26 @@ def get_containers_data():
 
 
 def main():
-    server = []
-    client = []
-
     gluster_data = get_containers_data()
     print "stop all"
     stop_all(gluster_data)
-
-    for item in gluster_data['data']:
-        if item['type'] == 'server':
-            server = item
-        else:
-            client = item
-
+    servers = gluster_data['data']['server']
+    client = gluster_data['data']['client']
+    web_proxy = gluster_data['data']['web']
     print "starting app"
     rc = up_vagrant()
     if rc == 0:
         print "start ok, link"
-        server_ip = link_servers(server, gluster_data['gluster'])
-        connect_client(server_ip, client, gluster_data['gluster'])
-    else:
+        server_ip = link_servers(servers, gluster_data['gluster'])
+        rc = connect_client(server_ip, client, gluster_data['gluster'])
+        if rc == 0:
+            print "All OK, glusterfs client listen on port %s" % web_proxy['port']
+            return 0
+        else:
+            print "Client link fail\n"
         print "start fail stop all"
         stop_all(gluster_data)
-
-    return 1
-
+        return 1
 
 if __name__ == '__main__':
     main()
