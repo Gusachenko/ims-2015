24393a6483d6e714c637bd6471bb933762735e09
Vladislav <farmatholin@gmail.com>
2015-12-27 19:48:02 +0300

    server up thread

---
+++ src/ServerUpThread.py
@@ -0,0 +1,45 @@
+import threading
+from util import *
+import paramiko
+import yaml
+from subprocess import Popen, PIPE
+from time import sleep
+import sys
+
+
+class ServerUpThread(threading.Thread):
+    def __init__(self, thread_id, name, image, key, node):
+        threading.Thread.__init__(self)
+        self.thread_id = thread_id
+        self.result = False
+        self.key = key
+        self.node = node
+        self.name = name
+        self.image = image
+
+    def return_code(self):
+        return self.thread_id, self.result
+
+    def run(self):
+        try:
+            run_cmd = DOCKER \
+                + " run -d --privileged=true -v /mnt/gluster_volume:/gluster_volume --name='" \
+                + self.name \
+                + "' --net='host' " \
+                + self.image
+            cmd = [
+                DOCKER + " rm -f " + self.name,
+                run_cmd
+            ]
+
+            exit_code = server_run_cmd(
+                self.node['ip'],
+                self.node['user'],
+                self.key,
+                cmd,
+            )
+            if exit_code is 0:
+                self.result = True
+                self.node['status'] = 1
+        except:
+            self.node['status'] = 0
